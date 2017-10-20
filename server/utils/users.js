7fdca5db41d49f74126de7a2eede56d5acd56577
Abdullahi Deji Ibrahim <abdul00385@yahoo.com>
2017-10-20 08:40:16 +0100

    Add Users class and updateUserList event

---
+++ server/utils/users.js
@@ -0,0 +1,57 @@
+[{
+  id: '/oregherpuiasd',
+  name: 'Deji',
+  room: 'The office Fans'
+}]
+
+//addUser(id, name, room)
+//removeUser(id)
+//getUser(id)
+//getUserList(room)
+
+class Users {
+  constructor() {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.getUser(id);
+
+    if (user) {
+       this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0];
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
+
+    return namesArray;
+  }
+}
+
+// class Person {
+//   constructor (name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//
+//   getUserDescription () {
+//     return `${this.name} is ${this.age} year(s) old.`;
+//   }
+//
+// }
+//
+// var me = new Person('Deji', 21);
+// var description = me.getUserDescription();
+// console.log(description);
+
+module.exports = {Users};
